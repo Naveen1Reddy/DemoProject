69c9bfb250ca2e63b60fa5a512eb2e39ce3b8583
M-Naveen-R <maarthanaveen228@gmail.com>
2022-05-02 23:26:50 +0530

    problems to be done On stacks

--- stacks.py
+++ stacks.py
@@ -3,4 +3,12 @@ Nearest Smaller Element on Left and Right side of an Element
 
 Nearest Greater Element of Left and Right Side of Every Element in the Array
 
+Paraentesis matching Problem
+
+Largest Area Of A Histogram
+
+Maximum rectangluar Area in a Binary Matrix with 1's or 0's
+
+Infix ,Prefix, Postfix
+
 """
